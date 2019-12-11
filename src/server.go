4893bb9900ce99ddf084baa5ccc0604865deca9e
zhilong <zhilong_l@yahoo.com>
2019-12-11 16:13:44 -0500

    basic dependency check

--- src/server.go
+++ src/server.go
@@ -21,6 +21,8 @@ type Server struct {
 	latestMsgID        int //starts from 0, i.e if we receive something with 2 first, then wait until received 1, then commit both
 	connLocalServers   map[int]net.Conn
 	localServerReaders map[int]*bufio.Reader
+	noDependency       string //"true" if no dependency issues, "false" otherwise. i.e. if this server is "blocking" then false
+	msgQueue           [][]string
 }
 
 func (self *Server) Run() {
@@ -38,6 +40,13 @@ func (self *Server) Run() {
 
 	go self.ListenMaster(connMaster)
 
+	localFacingPort := strconv.Itoa(25000 + self.sid%1000 + self.did)
+	lLocal, errL := net.Listen(CONNECT_TYPE, CONNECT_HOST+":"+localFacingPort)
+	if errL != nil {
+		fmt.Println("Error while listening to local connection")
+	}
+	go self.HandleLocal(lLocal)
+
 	lClient, errC := net.Listen(CONNECT_TYPE, CONNECT_HOST+":"+self.clientFacingPort)
 	if errC != nil {
 		fmt.Println("error listeining to client")
@@ -48,16 +57,16 @@ func (self *Server) Run() {
 		fmt.Println("Error while accepting connection")
 	}
 
-	//go self.HandleLocal(lLocal)
 	self.HandleClient(connClient, connMaster)
 
 }
+
 func (self *Server) DepCheck(receivedLC int) bool {
 	// return a bool, if True, then no dependency, can continue
 	if receivedLC > self.lClock+1 {
 		return false
 	} else {
-		myIdx := self.did % 1000 //the index of this server
+		myIdx := self.sid % 1000 //the index of this server
 		for i := 0; i < self.numPartitions; i++ {
 			if i != myIdx { // for all the other servers in this datacenter
 				if _, ok := self.connLocalServers[i]; !ok {
@@ -71,17 +80,20 @@ func (self *Server) DepCheck(receivedLC int) bool {
 				otherServerConn := self.connLocalServers[i]
 				otherServerConn.Write([]byte("dep_check\n"))
 				// wait for dep respionse from other server
-				reader := bufio.NewReader(otherServerConn)
-				otherServerReply, _ := reader.ReadString('\n')
-				//	fmt.Println("MESSAGE FROM CLIENT " + message)
+				if _, ok := self.localServerReaders[i]; !ok {
+					reader := bufio.NewReader(otherServerConn)
+					self.localServerReaders[i] = reader
+				}
+				localReader := self.localServerReaders[i]
+				otherServerReply, _ := localReader.ReadString('\n')
 				otherServerReply = strings.TrimSuffix(otherServerReply, "\n") // string of true or false
 				if otherServerReply == "false" {
 					return false
 				}
 			}
 		}
+		return true
 	}
-	return true
 }
 
 func (self *Server) ListenMaster(connMaster net.Conn) {
@@ -99,6 +111,11 @@ func (self *Server) ListenMaster(connMaster net.Conn) {
 		receivedKey := messageSlice[0]
 		receivedValue := messageSlice[1]
 		receivedLC, _ := strconv.Atoi(messageSlice[2])
+		if receivedLC > self.lClock+1 || len(self.msgQueue) > 0 {
+			self.noDependency = "false" // This is purely local dependency, not considering the other servers
+		} else {
+			self.noDependency = "true"
+		}
 		//check for dependency first
 		if self.DepCheck(receivedLC) {
 			if _, ok := self.kvStore[receivedKey]; !ok {
@@ -106,6 +123,24 @@ func (self *Server) ListenMaster(connMaster net.Conn) {
 			} else {
 				self.kvStore[receivedKey] = append(self.kvStore[receivedKey], receivedValue)
 			}
+			self.lClock += 1
+			// then clear the rest of the messages held in queue
+			for _, msgTuple := range self.msgQueue { //TODO: might want to make it more robust by doing multiple rounds of this? (potentially recursive)
+				msgKey := msgTuple[0]
+				msgValue := msgTuple[1]
+				msgLC, _ := strconv.Atoi(msgTuple[2])
+				if self.DepCheck(msgLC) {
+					if _, ok := self.kvStore[msgKey]; !ok {
+						self.kvStore[msgKey] = []string{msgValue}
+					} else {
+						self.kvStore[msgKey] = append(self.kvStore[msgKey], msgValue)
+					}
+					self.lClock += 1
+				}
+			}
+
+		} else { // add this message to the queue
+			self.msgQueue = append(self.msgQueue, messageSlice)
 		}
 
 	}
@@ -176,14 +211,22 @@ func (self *Server) HandleClient(connClient net.Conn, connMaster net.Conn) {
 	}
 }
 
-/*
 func (self *Server) HandleLocal(lLocal net.Listener) {
 	defer lLocal.Close()
-	for {
-		connLocal, errL := lLocal.Accept()
-		if errL != nil {
-			fmt.Println("error listeining to client")
-		}
-
+	connLocal, errL := lLocal.Accept()
+	if errL != nil {
+		fmt.Println("error accepting local connection")
 	}
-}*/
+	reader := bufio.NewReader(connLocal)
+	for {
+		message, _ := reader.ReadString('\n')
+		message = strings.TrimSuffix(message, "\n")
+		if message == "dep_check" { //carry out local dep check
+			connLocal.Write([]byte(self.noDependency + "\n"))
+		} else {
+			fmt.Println("invalid message received from the other local server")
+			continue
+		}
+	}
+
+}
